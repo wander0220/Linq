99f6dc4e095866e9f2714b127799546bc26a9b0d
wander0220 <s2018s26@e-mirim.hs.kr>
2020-10-06 09:47:04 +0900

    익명객체

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -14,11 +14,30 @@ namespace Linq
                 new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var output = from i in input
                          where i>5 && i%2 == 0 
+                         orderby i ascending
                          select i;
 
             foreach( var i in output)
             {
                 Console.WriteLine(i);
+            }
+
+            Console.WriteLine("####################");
+
+
+            var output2 = from i in input
+                          where i % 2 == 0
+                          select new
+                          {
+                              A = i * 2,
+                              B = i * i,
+                              C = 100,
+                              name = "사람" + i
+                          };
+
+            foreach(var i in output2)
+            {
+                Console.WriteLine(i.A);
             }
 
         }
