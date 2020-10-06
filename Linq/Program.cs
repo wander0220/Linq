841e6e610c9290784da8138942a304b95cca88b4
wander0220 <s2018s26@e-mirim.hs.kr>
2020-10-06 09:36:24 +0900

    초기 커밋.

---
+++ Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input =
+                new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var output = from i in input
+                         where i>5 && i%2 == 0 
+                         select i;
+
+            foreach( var i in output)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
